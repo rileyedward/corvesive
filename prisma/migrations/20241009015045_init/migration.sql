1d0fb7ea4c7216fdd77dfb63abb12ed1d1bdef89
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-08 20:51:54 -0500

    prisma init

---
+++ prisma/migrations/20241009015045_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
