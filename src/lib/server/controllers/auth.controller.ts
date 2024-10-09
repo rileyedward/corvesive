a135857583e9191f759f6c28fc3f5e4038babdde
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-08 21:47:30 -0500

    prettier

--- src/lib/server/controllers/auth.controller.ts
+++ src/lib/server/controllers/auth.controller.ts
@@ -1,18 +1,18 @@
-import prisma from "$lib/server/db";
+import prisma from '$lib/server/db';
 
 export const RegisterController = async (
-  first_name: string,
-  last_name: string,
-  email: string,
-  password: string,
-  password_confirm: string
+	first_name: string,
+	last_name: string,
+	email: string,
+	password: string,
+	password_confirm: string
 ) => {
-  await prisma.users.create({
-    data: {
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password: password
-    }
-  });
+	await prisma.users.create({
+		data: {
+			first_name: first_name,
+			last_name: last_name,
+			email: email,
+			password: password
+		}
+	});
 };
--- src/routes/(auth)/register/+page.server.ts
+++ src/routes/(auth)/register/+page.server.ts
@@ -1,9 +1,9 @@
-import type { Actions } from "@sveltejs/kit";
+import type { Actions } from '@sveltejs/kit';
 
 export const actions = {
-  default: async () => {
-    return {
-      message: 'This is the default action'
-    }
-  }
+	default: async () => {
+		return {
+			message: 'This is the default action'
+		};
+	}
 } satisfies Actions;
