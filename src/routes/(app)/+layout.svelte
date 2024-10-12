f23896d19639269869201fcab317f2810784e567
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-12 16:46:58 -0500

    wip

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,9 @@
 <div class="max-w-4xl mx-auto px-4">
   <nav class="flex justify-between items-center py-4 mb-4">
     <div>
-      <h1 class="text-2xl font-bold">Corvesive</h1>
+      <a href="/dashboard">
+        <h1 class="text-2xl font-bold transform hover:scale-105">Corvesive</h1>
+      </a>
     </div>
 
     <div>
