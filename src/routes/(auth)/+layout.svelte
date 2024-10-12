0205c050737a594a4d5318f66f9b3f96d87cf4f3
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-12 15:13:08 -0500

    wip

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,5 +1,5 @@
-<div>
-  <h1 class="text-3xl text-center">Corvesive</h1>
+<div class="max-w-22xl mx-auto px-4">
+  <h1 class="text-3xl text-center mb-2">Corvesive</h1>
   <div class="border border-black">
     <slot />
   </div>
