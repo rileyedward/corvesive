bd680b8b7c7f4f38a58064794e5738e2aea4884e
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-12 16:45:06 -0500

    wip

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="max-w-4xl mx-auto px-4">
+  <nav class="flex justify-between items-center py-4 mb-4">
+    <div>
+      <h1 class="text-2xl font-bold">Corvesive</h1>
+    </div>
+
+    <div>
+      <p>logout</p>
+    </div>
+  </nav>
+
+  <slot />
+</div>
