3a957565529fc26cb4471ac0055b35b439cc6dc6
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-12 17:14:54 -0500

    wip

---
+++ src/lib/components/Construction.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center p-6">
+	<div class="text-center space-y-8 p-6">
+		<h1 class="text-4xl font-bold">🚧 Page Under Construction 🚧</h1>
+		<p class="text-lg">
+			We're working hard to bring something awesome to this page. Stay tuned for more to come soon!
+		</p>
+	</div>
+</div>
