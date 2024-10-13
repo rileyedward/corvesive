19f97e1bb85a12ea851a2f3e95fe2a798fcff2f2
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-13 00:20:19 -0500

    wip

---
+++ src/lib/server/controllers/PaystubRecordController.ts
@@ -0,0 +1,74 @@
+import type { TApiResponse } from "$lib/types/ApiTypes";
+import prisma from "$lib/server/db";
+import { AuthorizationError, ValidationErrors } from "$lib/server/helpers/ErrorHelper";
+import { ReschedulePaystubRequest, UpdatePaystubRequest, type TReschedulePaystubRequest, type TUpdatePaystubRequest } from "$lib/server/requests/PaystubRequest";
+
+export async function ReschedulePaystub(
+  payload: TReschedulePaystubRequest,
+  paystub_record_id: number,
+  user_id: number
+): Promise<TApiResponse> {
+  const validationErrors = ReschedulePaystubRequest(payload);
+	if (validationErrors.length) {
+		return ValidationErrors(validationErrors);
+	}
+
+  const paystubRecord = await prisma.paystub_records.findFirst({
+    where: {
+      id: paystub_record_id,
+      user_id: user_id
+    }
+  });
+
+  if (!paystubRecord) {
+    return AuthorizationError();
+  }
+
+  await prisma.paystub_records.update({
+    where: {
+      id: paystubRecord.id
+    },
+    data: {
+      pay_day: payload.pay_day
+    }
+  });
+
+  return {
+    message: 'Paystub rescheduled'
+  };
+}
+
+export async function UpdatePaystubRecord(
+  payload: TUpdatePaystubRequest,
+  paystub_record_id: number,
+  user_id: number
+): Promise<TApiResponse> {
+const validationErrors = UpdatePaystubRequest(payload);
+	if (validationErrors.length) {
+		return ValidationErrors(validationErrors);
+	}
+
+  const paystubRecord = await prisma.paystub_records.findFirst({
+    where: {
+      id: paystub_record_id,
+      user_id: user_id
+    }
+  });
+
+  if (!paystubRecord) {
+    return AuthorizationError();
+  }
+
+  await prisma.paystub_records.update({
+    where: {
+      id: paystubRecord.id
+    },
+    data: {
+      amount_in_cents: payload.amount_in_cents
+    }
+  });
+
+  return {
+    message: 'Paystub updated'
+  };
+}
--- src/lib/server/requests/PaystubRequest.ts
+++ src/lib/server/requests/PaystubRequest.ts
@@ -9,6 +9,14 @@ export type TPaystubRequest = {
 	recurrence_interval_two?: number;
 };
 
+export type TReschedulePaystubRequest = {
+	pay_day: Date;
+}
+
+export type TUpdatePaystubRequest = {
+	amount_in_cents: number;
+};
+
 export const PaystubSchema = z.object({
 	issuer: z.string().min(1).max(50),
 	amount_in_cents: z.number().int().positive(),
@@ -17,8 +25,34 @@ export const PaystubSchema = z.object({
 	recurrence_interval_two: z.number().int().min(1).max(31).optional()
 });
 
+export const ReschedulePaystubSchema = z.object({
+	pay_day: z.date()
+});
+
+export const UpdatePaystubSchema = z.object({
+	amount_in_cents: z.number().int().positive()
+});
+
 export function PaystubRequest(payload: TPaystubRequest): string[] {
 	const result = PaystubSchema.safeParse(payload);
+	if (!result.success) {
+		return formatZodErrors(result.error.issues);
+	}
+
+	return [];
+}
+
+export function ReschedulePaystubRequest(payload: TReschedulePaystubRequest): string[] {
+	const result = ReschedulePaystubSchema.safeParse(payload);
+	if (!result.success) {
+		return formatZodErrors(result.error.issues);
+	}
+
+	return [];
+}
+
+export function UpdatePaystubRequest(payload: TUpdatePaystubRequest): string[] {
+	const result = UpdatePaystubSchema.safeParse(payload);
 	if (!result.success) {
 		return formatZodErrors(result.error.issues);
 	}
