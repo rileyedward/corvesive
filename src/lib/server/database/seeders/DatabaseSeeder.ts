3c2d7d4de8e181deb405beda4103c0b95cfd9b07
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-13 11:25:40 -0500

    wip

--- src/lib/server/database/seeders/DatabaseSeeder.ts
+++ src/lib/server/database/seeders/DatabaseSeeder.ts
@@ -1,18 +1,11 @@
 import prisma from '$lib/server/database/db';
 import { PaystubFactory } from '$lib/server/database/factories/PaystubFactory';
 import { scheduleFuturePaystubs } from '$lib/server/services/PaystubScheduler';
+import { UserFactory } from '../factories/UserFactory';
 
 export async function DatabaseSeeder() {
 	try {
-		const user = await prisma.users.create({
-			data: {
-				first_name: 'Riley',
-				last_name: 'Grotenhuis',
-				email: 'rileygrotenhuis@gmail.com',
-				password: 'password'
-			}
-		});
-
+		const user = await UserFactory();
 		const paystub = await PaystubFactory(user, 'bi-weekly');
 
 		await scheduleFuturePaystubs(paystub);
