0237afd21cd990df3d1f637fcd7494874b39ab3d
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-13 10:04:50 -0500

    wip

---
+++ src/lib/server/controllers/ExpenseRecordController.ts
@@ -0,0 +1,79 @@
+import prisma from '$lib/server/db';
+import {
+	RescheduleExpenseRequest,
+	UpdateExpenseRequest,
+	type TRescheduleExpenseRequest,
+	type TUpdateExpenseRequest
+} from '$lib/server/requests/ExpenseRequest';
+import type { TApiResponse } from '$lib/types/ApiTypes';
+import { AuthorizationError, ValidationErrors } from '$lib/server/helpers/ErrorHelper';
+
+export async function RescheduleExpense(
+	payload: TRescheduleExpenseRequest,
+	expense_record_id: number,
+	user_id: number
+): Promise<TApiResponse> {
+	const validationErrors = RescheduleExpenseRequest(payload);
+	if (validationErrors.length) {
+		return ValidationErrors(validationErrors);
+	}
+
+	const expenseRecord = await prisma.expense_records.findFirst({
+		where: {
+			id: expense_record_id,
+			user_id: user_id
+		}
+	});
+
+	if (!expenseRecord) {
+		return AuthorizationError();
+	}
+
+	await prisma.expense_records.update({
+		where: {
+			id: expenseRecord.id
+		},
+		data: {
+			due_date: payload.due_date
+		}
+	});
+
+	return {
+		message: 'Expense rescheduled'
+	};
+}
+
+export async function UpdateExpenseRecord(
+	payload: TUpdateExpenseRequest,
+	expense_record_id: number,
+	user_id: number
+): Promise<TApiResponse> {
+	const validationErrors = UpdateExpenseRequest(payload);
+	if (validationErrors.length) {
+		return ValidationErrors(validationErrors);
+	}
+
+	const expenseRecord = await prisma.expense_records.findFirst({
+		where: {
+			id: expense_record_id,
+			user_id: user_id
+		}
+	});
+
+	if (!expenseRecord) {
+		return AuthorizationError();
+	}
+
+	await prisma.expense_records.update({
+		where: {
+			id: expenseRecord.id
+		},
+		data: {
+			amount_in_cents: payload.amount_in_cents
+		}
+	});
+
+	return {
+		message: 'Expense record updated'
+	};
+}
--- src/lib/server/requests/ExpenseRequest.ts
+++ src/lib/server/requests/ExpenseRequest.ts
@@ -2,59 +2,59 @@ import { z } from 'zod';
 import { formatZodErrors } from '../helpers/ErrorHelper';
 
 export type TExpenseRequest = {
-  issuer: string;
-  name: string;
-  due_day_of_month: number;
-  amount_in_cents: number;
-  is_variable: boolean;
+	issuer: string;
+	name: string;
+	due_day_of_month: number;
+	amount_in_cents: number;
+	is_variable: boolean;
 };
 
 export type TRescheduleExpenseRequest = {
-  due_date: Date;
+	due_date: Date;
 };
 
 export type TUpdateExpenseRequest = {
-  amount_in_cents: number;
+	amount_in_cents: number;
 };
 
 export const ExpenseSchema = z.object({
-  issuer: z.string().min(1).max(50),
-  name: z.string().min(1).max(50),
+	issuer: z.string().min(1).max(50),
+	name: z.string().min(1).max(50),
 	amount_in_cents: z.number().int().positive(),
-  is_variable: z.boolean(),
+	is_variable: z.boolean()
 });
 
 export const RescheduleExpenseSchema = z.object({
-  due_date: z.date()
+	due_date: z.date()
 });
 
 export const UpdateExpenseSchema = z.object({
-  amount_in_cents: z.number().int().positive()
+	amount_in_cents: z.number().int().positive()
 });
 
 export function ExpenseRequest(payload: TExpenseRequest): string[] {
-  const result = ExpenseSchema.safeParse(payload);
-  if (!result.success) {
-    return formatZodErrors(result.error.issues);
-  }
+	const result = ExpenseSchema.safeParse(payload);
+	if (!result.success) {
+		return formatZodErrors(result.error.issues);
+	}
 
-  return [];
+	return [];
 }
 
 export function RescheduleExpenseRequest(payload: TRescheduleExpenseRequest): string[] {
-  const result = RescheduleExpenseSchema.safeParse(payload);
-  if (!result.success) {
-    return formatZodErrors(result.error.issues);
-  }
+	const result = RescheduleExpenseSchema.safeParse(payload);
+	if (!result.success) {
+		return formatZodErrors(result.error.issues);
+	}
 
-  return [];
+	return [];
 }
 
 export function UpdateExpenseRequest(payload: TUpdateExpenseRequest): string[] {
-  const result = UpdateExpenseSchema.safeParse(payload);
-  if (!result.success) {
-    return formatZodErrors(result.error.issues);
-  }
+	const result = UpdateExpenseSchema.safeParse(payload);
+	if (!result.success) {
+		return formatZodErrors(result.error.issues);
+	}
 
-  return [];
+	return [];
 }
