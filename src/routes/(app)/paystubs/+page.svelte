8324aa574692659128aba8fd6b072f63cfcb6fbd
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-13 13:07:06 -0500

    wip

--- src/routes/(app)/paystubs/+page.svelte
+++ src/routes/(app)/paystubs/+page.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import { goto, invalidate } from '$app/navigation';
-	import FormErrors from '$lib/components/FormErrors.svelte';
-	import Modal from '$lib/components/Modal.svelte';
+	import CreatePaystubForm from '$lib/components/paystubs/CreatePaystubForm.svelte';
 	import type { ActionData } from './$types';
 
 	export let data;
 	export let form: ActionData;
 
 	let showForm: boolean = false;
-	let recurrence_rate: 'weekly' | 'bi-weekly' | 'monthly' | 'semi-monthly' = 'weekly';
 </script>
 
 <div class="flex flex-col md:flex-row justify-between items-start gap-4">
@@ -71,89 +67,4 @@
 	</div>
 </div>
 
-<Modal title="Add new paystub" show={showForm} close={() => (showForm = false)}>
-	<div>
-		<p class="max-w-xl text-xs mb-4">
-			Track your monthly income by adding a new paystub. This will help you better plan your monthly
-			budget.
-		</p>
-
-		<form
-			class="space-y-4"
-			method="POST"
-			use:enhance={() => {
-				return async ({ result, update }) => {
-					if (result.type === 'success') {
-						invalidate('paystubs');
-						showForm = false;
-					}
-
-					update();
-				};
-			}}
-		>
-			<div>
-				<label for="issuer">Issuer</label>
-				<input type="text" name="issuer" class="w-full" />
-			</div>
-
-			<div>
-				<label for="amount_in_cents">Amount</label>
-				<input type="number" name="amount_in_cents" class="w-full" />
-			</div>
-
-			<div>
-				<label for="recurrence_rate">Recurrence Rate</label>
-				<select name="recurrence_rate" class="w-full" bind:value={recurrence_rate}>
-					<option value="weekly">Weekly</option>
-					<option value="bi-weekly">Bi-Weekly</option>
-					<option value="monthly">Monthly</option>
-					<option value="semi-monthly">Semi-Monthly</option>
-				</select>
-			</div>
-
-			{#if recurrence_rate === 'weekly' || recurrence_rate === 'bi-weekly'}
-				<div>
-					<label for="recurrence_interval_one">Interval One</label>
-					<select name="recurrence_interval_one" class="w-full">
-						<option value={1}>Sunday</option>
-						<option value={2}>Monday</option>
-						<option value={3}>Tuesday</option>
-						<option value={4}>Wednesday</option>
-						<option value={5}>Thursday</option>
-						<option value={6}>Friday</option>
-						<option value={7}>Saturday</option>
-					</select>
-				</div>
-			{/if}
-
-			{#if recurrence_rate === 'monthly' || recurrence_rate === 'semi-monthly'}
-				<div>
-					<label for="recurrence_interval_one">Interval One</label>
-					<select name="recurrence_interval_one" class="w-full">
-						{#each Array.from({ length: 28 }, (_, i) => i + 1) as day}
-							<option value={day}>{day}</option>
-						{/each}
-					</select>
-				</div>
-			{/if}
-
-			{#if recurrence_rate === 'semi-monthly'}
-				<div>
-					<label for="recurrence_interval_two">Interval Two</label>
-					<select name="recurrence_interval_two" class="w-full">
-						{#each Array.from({ length: 28 }, (_, i) => i + 1) as day}
-							<option value={day}>{day}</option>
-						{/each}
-					</select>
-				</div>
-			{/if}
-
-			<FormErrors {form} />
-
-			<div class="flex justify-end">
-				<button type="submit" class="btn btn-primary">Save</button>
-			</div>
-		</form>
-	</div>
-</Modal>
+<CreatePaystubForm {form} bind:showForm />
