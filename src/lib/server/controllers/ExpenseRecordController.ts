59e433384c823b0de2e12e79136d4a508ba45aba
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-13 19:25:29 -0500

    wip

--- src/lib/server/controllers/ExpenseRecordController.ts
+++ src/lib/server/controllers/ExpenseRecordController.ts
@@ -1,73 +1,10 @@
 import prisma from '$lib/server/database/db';
 import {
-	RescheduleExpenseRequest,
 	UpdateExpenseRequest,
-	type TRescheduleExpenseRequest,
 	type TUpdateExpenseRequest
 } from '$lib/server/requests/ExpenseRequest';
 import type { TApiResponse } from '$lib/types/ApiTypes';
 import { AuthorizationError, ValidationErrors } from '$lib/server/helpers/ErrorHelper';
-
-export async function RescheduleExpense(
-	payload: TRescheduleExpenseRequest,
-	expense_record_id: number,
-	user_id: number
-): Promise<TApiResponse> {
-	const validationErrors = RescheduleExpenseRequest(payload);
-	if (validationErrors.length) {
-		return ValidationErrors(validationErrors);
-	}
-
-	const expenseRecord = await prisma.expense_records.findFirst({
-		where: {
-			id: expense_record_id,
-			user_id: user_id
-		}
-	});
-
-	if (!expenseRecord) {
-		return AuthorizationError();
-	}
-
-	await prisma.expense_records.update({
-		where: {
-			id: expenseRecord.id
-		},
-		data: {
-			due_date: payload.due_date
-		}
-	});
-
-	return {
-		message: 'Expense rescheduled'
-	};
-}
-
-export async function UnscheduleExpense(
-	expense_record_id: number,
-	user_id: number
-): Promise<TApiResponse> {
-	const expenseRecord = await prisma.expense_records.findFirst({
-		where: {
-			id: expense_record_id,
-			user_id: user_id
-		}
-	});
-
-	if (!expenseRecord) {
-		return AuthorizationError();
-	}
-
-	await prisma.expense_records.delete({
-		where: {
-			id: expenseRecord.id
-		}
-	});
-
-	return {
-		message: 'Expense unscheduled'
-	};
-}
 
 export async function UpdateExpenseRecord(
 	payload: TUpdateExpenseRequest,
@@ -103,5 +40,31 @@ export async function UpdateExpenseRecord(
 
 	return {
 		message: 'Expense record updated'
+	};
+}
+
+export async function UnscheduleExpense(
+	expense_record_id: number,
+	user_id: number
+): Promise<TApiResponse> {
+	const expenseRecord = await prisma.expense_records.findFirst({
+		where: {
+			id: expense_record_id,
+			user_id: user_id
+		}
+	});
+
+	if (!expenseRecord) {
+		return AuthorizationError();
+	}
+
+	await prisma.expense_records.delete({
+		where: {
+			id: expenseRecord.id
+		}
+	});
+
+	return {
+		message: 'Expense unscheduled'
 	};
 }
