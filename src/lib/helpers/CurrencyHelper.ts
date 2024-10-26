f7405ec7b667c1016cd6da455bdecf492be40381
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-26 13:42:26 -0500

    better money formatting

--- src/lib/helpers/CurrencyHelper.ts
+++ src/lib/helpers/CurrencyHelper.ts
@@ -1,5 +1,8 @@
 export function CentsToDollars(cents: number): string {
-	return (cents / 100).toFixed(2);
+	return (cents / 100).toLocaleString('en-US', {
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2
+	});
 }
 
 export function CentsToDollarsPretty(cents: number): string {
@@ -7,5 +10,5 @@ export function CentsToDollarsPretty(cents: number): string {
 }
 
 export function DollarsToCents(dollars: number): number {
-	return dollars * 100;
+	return Math.round(dollars * 100);
 }
