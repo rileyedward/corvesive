d0195c3f24020cc8671407a8b2e579507e2f1486
Riley Grotenhuis <riley@Mac.attlocal.net>
2024-10-26 13:41:03 -0500

    show the correct timezone from what was entered

--- src/lib/helpers/DateHelper.ts
+++ src/lib/helpers/DateHelper.ts
@@ -2,7 +2,8 @@ export function ShortDate(date: Date): string {
 	return date.toLocaleDateString('en-US', {
 		month: '2-digit',
 		day: '2-digit',
-		year: 'numeric'
+		year: 'numeric',
+		timeZone: 'UTC'
 	});
 }
 
@@ -10,12 +11,14 @@ export function FullDate(date: Date): string {
 	return date.toLocaleDateString('en-US', {
 		month: 'long',
 		day: 'numeric',
-		year: 'numeric'
+		year: 'numeric',
+		timeZone: 'UTC'
 	});
 }
 
 export function DayOfWeek(day: number): string {
 	return new Date(0, 0, day).toLocaleDateString('en-US', {
-		weekday: 'long'
+		weekday: 'long',
+		timeZone: 'UTC'
 	});
 }
